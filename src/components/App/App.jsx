fdaf180ccf2ad5633868c11c076cdc2d1e1f4bd8
Courtney Azar <courtney.a.azar@gmail.com>
2022-06-10 10:14:20 -0500

    styling completed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,20 +5,70 @@ function App() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
+          <h1 className="App-title">Photos of the Year Gallery</h1>
         </header>
-        <p>Which photos in the gallery are your favorite? Hit the 'like' button to vote!</p>
-        <div>
-          <img src="images/goat_small.jpg"/>
-          <img src="images/African-Sunset.webp"/>
-          <img src="images/Butterfly.webp"/>
-          <img src="images/Circle-river.jpeg"/>
-          <img src="images/Colorful-Mountain.webp"/>
-          <img src="images/Mountain-in-red.jpeg"/>
-          <img src="images/Ocean-rock.jpeg"/>
-          <img src="images/Raindrop.webp"/>
-          <img src="images/Tree-by-water.jpeg"/>
-          <img src="images/Winter-trees.jpeg"/>
+        <p>Which photos in the gallery are your favorite? Press the 'like' button to vote!</p>
+        <div id="theGallery">
+          <span>
+            <img src="images/goat_small.jpg"/>
+            <p>Description: Goat on a mountainside.</p>
+            <p id="numberOfLikes">Image Likes:</p>
+            <button id="likeBtn">Like</button>
+          </span>
+          <span>
+            <img src="images/African-Sunset.webp"/>
+            <p>Description: African sunset.</p>
+            <p id="numberOfLikes">Image Likes:</p>
+            <button id="likeBtn">Like</button>
+          </span>
+          <span>
+            <img src="images/Butterfly.webp"/>
+            <p>Description: Butterfly on a flower.</p>
+            <p id="numberOfLikes">Image Likes:</p>
+            <button id="likeBtn">Like</button>
+          </span>
+          <span>
+            <img src="images/Circle-river.jpeg"/>
+            <p>Description: Bridge on a river.</p>
+            <p id="numberOfLikes">Image Likes:</p>
+            <button id="likeBtn">Like</button>
+          </span>
+          <span>
+            <img src="images/Colorful-Mountain.webp"/>
+            <p>Description: Colorful mountain by a lake.</p>
+            <p id="numberOfLikes">Image Likes:</p>
+            <button id="likeBtn">Like</button>
+          </span>
+          <span>
+            <img src="images/Mountain-in-red.jpeg"/>
+            <p>Description: Mountain in a red forest.</p>
+            <p id="numberOfLikes">Image Likes:</p>
+            <button id="likeBtn">Like</button>
+          </span>
+          <span>
+            <img src="images/Ocean-rock.jpeg"/>
+            <p>Description: Giant rock on the ocean.</p>
+            <p id="numberOfLikes">Image Likes:</p>
+            <button id="likeBtn">Like</button>
+          </span>
+          <span>
+            <img src="images/Raindrop.webp"/>
+            <p>Description: Raindrop.</p>
+            <p id="numberOfLikes">Image Likes:</p>
+            <button id="likeBtn">Like</button>
+          </span>
+          <span>
+            <img src="images/Tree-by-water.jpeg"/>
+            <p>Description: Tree by the water.</p>
+            <p id="numberOfLikes">Image Likes:</p>
+            <button id="likeBtn">Like</button>
+          </span>
+          <span>
+            <img src="images/Winter-trees.jpeg"/>
+            <p>Description: Winter trees frozen in time.</p>
+            <p id="numberOfLikes">Image Likes:</p>
+            <button id="likeBtn">Like</button>
+          </span>
         </div>
       </div>
     );
