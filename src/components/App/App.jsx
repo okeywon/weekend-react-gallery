6a37b1d1a327f8827eda27ed093583b4cf2fa914
Courtney Azar <courtney.a.azar@gmail.com>
2022-06-10 09:58:01 -0500

    base mode set up complete

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,8 +7,19 @@ function App() {
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <p>Which photos in the gallery are your favorite? Hit the 'like' button to vote!</p>
+        <div>
+          <img src="images/goat_small.jpg"/>
+          <img src="images/African-Sunset.webp"/>
+          <img src="images/Butterfly.webp"/>
+          <img src="images/Circle-river.jpeg"/>
+          <img src="images/Colorful-Mountain.webp"/>
+          <img src="images/Mountain-in-red.jpeg"/>
+          <img src="images/Ocean-rock.jpeg"/>
+          <img src="images/Raindrop.webp"/>
+          <img src="images/Tree-by-water.jpeg"/>
+          <img src="images/Winter-trees.jpeg"/>
+        </div>
       </div>
     );
 }
